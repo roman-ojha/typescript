3016325075469a86500a0e4056c84272270d1006
Roman Ojha <razzroman98@gmail.com>
2022-03-28 23:23:37 +0545

    typescript class on compiling typescript

---
+++ Class/Topic_Wise/02_Compiling_TypeScript/sandbox.ts
@@ -0,0 +1,8 @@
+const character = "Roman";
+console.log(character);
+const inputs = document.querySelectorAll("input");
+// as we can see that we can write all javascript code in typescript code as well
+inputs.forEach((input) => {
+  console.log(input);
+  console.log("hello");
+});
