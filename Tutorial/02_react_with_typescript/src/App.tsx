ebf37aed0a3d32d4e98bd0a8ceca520d5cce809e
Roman Ojha <razzroman98@gmail.com>
2022-03-31 20:06:11 +0545

    react with ts tut, added all of the component and displaying component on app.tsx

--- Tutorial/02_react_with_typescript/src/App.tsx
+++ Tutorial/02_react_with_typescript/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import List from "./react-components/List";
+import List from "./react-components/List";
 import AddToList from "./react-components/AddToList";
 import "./App.css";
 
@@ -16,7 +16,54 @@ export interface IState {
 }
 
 function App() {
-  return <div className="App"></div>;
+  const [number, setNumber] = useState<number | boolean>(5);
+  // to set our new state as some specific type then we have to use <<type_name>>
+  // now this number is of type 'number' or 'boolean'
+  const changeNumber = () => {
+    // setNumber("10");
+    // so here we can't be able to assign 'string' type on to 'number'
+  };
+
+  // const [people, setPeople] = useState([
+  //   {
+  //     name: "LeBron James",
+  //     url: "",
+  //     age: 36,
+  //     note: "",
+  //   },
+  //   {
+  //     name:"Kobe Bryant",
+  //     url:"",
+  //     age:36,
+  //   }
+  // ]);
+
+  // const [people, setPeople] = useState<{
+  //   // we can easily initialize type like this but we would not do that rather we would use 'interface'
+  //   age: number;
+  //   note: string;
+  //   name: string;
+  //   url: string;
+  // }>();
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Lebron Jamse",
+      url: "https://media.gettyimages.com/photos/lebron-james-of-the-los-angeles-lakers-poses-for-a-portrait-during-picture-id1172287886?s=612x612",
+      age: 36,
+      note: "Allegeric: to staying on the same team",
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>People Invited to our Party</h1>
+      <List people={people} />
+      {/* here we are trying to pass state as an prop but if we will pass just like 'js' the we will get the error because 'List' component is not expecting to get props but we are passing it to solve the problem we can define that we are expecting props inside the 'List' component*/}
+      <AddToList people={people} setPeople={setPeople} />
+      {/* we want to add list throught 'AddToList' component because of that we have to pass 'people' and 'setPeople' as an props so that 'AddToList' can access to these variables and functions */}
+    </div>
+  );
 }
 
 export default App;
